e9fbc4bc9c27dbe39590bd71155505e1716cd78f
Jason Kao <jkao1@stuy.edu>
2016-04-18 18:17:42 -0400

    moved <scripts> in html into njc.js

--- new-jen-canvas/njc.js
+++ new-jen-canvas/njc.js
@@ -1,68 +1,78 @@
-/* game init */
-
 var char;
 
-function startGame() {
-    // upper left: (0,0)
-    char = new component(30, 30, 5, 265, "#f89406");
+function start() { // initiates game
+    char = new component(15, 15, "#fa8940", 20, 265); 
     area.start();
 }
 
-var area = {
+var area = { // setting up canvas and its properties
     canvas : document.createElement("canvas"),
     start : function() {
         this.canvas.width = 960;
         this.canvas.height = 560;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(update, 20);
+        this.interval = setInterval(updateArea, 20); // frames
+        // keyboard stuff
+        window.addEventListener('keydown', function (e) {
+            area.keys = (area.keys || []);
+            area.keys[e.keyCode] = (e.type == 'keydown');
+        })
+        window.addEventListener('keyup', function (e) {
+            area.keys[e.keyCode] = (e.type == 'keydown');
+        })
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
-function component(width, height, x, y, color) {
+function component(width, height, color, x, y, type) {
+    this.type = type; // for images
+    if (type == "image") {
+        this.image = new Image();
+        this.image.src = color;
+    } 
     this.width = width;
     this.height = height;
-    this.x = x;
-    this.y = y;    
-    this.speedX = 0;
-    this.speedY = 0;
-    this.update = function(){
+    this.x = x; // x pos
+    this.y = y; // y pos
+    this.angle = Math.PI/2;  // in radians
+    this.speed = 0; // character speed
+    this.angleInc = 0; // how much angle increments by
+    this.posDeg = function() {
         ctx = area.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.save(); // saves the entire canvas
+        // canvas receives char properties (loc, deg, color)
+        ctx.translate(this.x, this.y); 
+        ctx.rotate(this.angle);
+        if (type == "image") {
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        } else {
+            ctx.fillStyle = color;
+            ctx.fillRect(this.width / -2, this.height / -2, this.width, this.height);
+        }
+        // canvas spawns a duplicate char to its own properties
+        ctx.restore();
+        // canvas is restored to its last save but new char remains
     }
-    this.newPos = function() {
-        this.x += this.speedX;
-        this.y += this.speedY; 
-    } 
+    this.posXY = function() {
+        this.angle += this.angleInc * Math.PI / 180;
+        this.x += this.speed * Math.sin(this.angle);
+        this.y -= this.speed * Math.cos(this.angle);        
+    }    
 }
 
-function update() {
+function updateArea() {
     area.clear();
-    char.newPos();
-    char.update();
-}
-
-function moveup() {
-    char.speedY -= 1; 
-}
-
-function movedown() {
-    char.speedY += 1; 
-}
-
-function moveleft() {
-    char.speedX -= 1;
-}
-
-function moveright() {
-    char.speedX += 1;
-}
-
-function stopMove() {
-    char.speedX = 0;
-    char.speedY = 0; 
+    char.angleInc = 0;
+    char.speed = 1;
+    // space(32) controls
+    if (area.keys && area.keys[32]) {char.angleInc = 3; } 
+    /* 
+    alternate controls:
+    reg rotation: ( 左(37,+angleInc), 右(39,-angleInc), 上(40,-speed), 下(38,+speed))
+    */
+    char.posXY(); // updates 2d location  
+    char.posDeg(); // updates rotation factor
 }
