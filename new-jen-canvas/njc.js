20bb0fc04db4942207c1085d4b252b0d87f84ba6
Jason Kao <jkao1@stuy.edu>
2016-04-19 18:48:41 -0400

    stuff. works so far with getting on stuck on a border

--- new-jen-canvas/njc.js
+++ new-jen-canvas/njc.js
@@ -1,9 +1,9 @@
 var char;
-var obstacle;
+var obA;
 
 function start() { // initiates game
-    char = new component(15, 15, "#fa8940", 20, 265); 
-    obstacle  = new component(10, 200, "red",300, 480);
+    char = new component(15, 15, "#fa8940", 250, 265); 
+    obA = new component(200, 200, "red", 100, 100); 
     area.start();
 }
 
@@ -26,6 +26,9 @@ var area = { // setting up canvas and its properties
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height); 
+    },
+    stop : function() {
+        clearInterval(this.interval);
     }
 }
 
@@ -47,42 +50,90 @@ function component(width, height, color, x, y) {
         ctx.fillRect(this.width / -2, this.height / -2, this.width, this.height);
         // canvas spawns a duplicate char to its own properties
         ctx.restore();
+        
         // canvas is restored to its last save but new char remains
     }
     this.posXY = function() {
         this.angle += this.angleInc * Math.PI / 180;
         this.x += this.speed * Math.sin(this.angle);
         this.y -= this.speed * Math.cos(this.angle);        
-    }    
+    }  
+    this.altPos = function() {
+        this.angle += this.angleInc * Math.PI / 180;
+    }  
+    this.crashWith = function(otherobj) {
+        var myleft = this.x - (this.width / 2);
+        var myright = this.x + (this.width);
+        var mytop = this.y - (this.height / 2);
+        var mybottom = this.y + (this.height / 2);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width / 2);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height / 2);
+        var crash = true;
+        if ((mytop > otherbottom) || (myleft > otherright) || (myright < otherleft) || (mybottom < othertop)) {
+            crash = false;
+        }
+        return crash;/*
+        var myleft = this.x - (this.width / 2);
+        var myright = this.x + (this.width / 2);
+        var mytop = this.y - (this.height / 2);
+        var mybottom = this.y + (this.height / 2);
+        var objleft = obj.x - (obj.width / 2);
+        var objright = obj.x + (obj.width / 2);
+        var objtop = obj.y - (obj.height / 2);
+        var objbottom = obj.y + (obj.height / 2);
+        var crash = true;
+        if ((myleft < objleft) && (myright > objright) && (mytop > objtop) && (mybottom < objbottom)) {
+            crash = false;
+        }
+        return crash;*/
+    }
 }
 
 var inc = 0; // angleInc's increment
 var angleSpeed = 0;
 
 function updateArea() {
-    area.clear(); // so no duplicate char
-    char.angleInc = 0;
-    char.speed = 3;
-    if (area.keys && area.keys[32]) {
-        angleSpeed = 3 + inc;
-        char.angleInc = angleSpeed;
-        inc += 0.1; // the longer area.keys[32], the faster char spins
-    } else if (angleSpeed > 0) {
-        inc = 0;
-        angleSpeed -= 1;
-        char.angleInc = angleSpeed;
-    } 
-    /* 
-    alternate controls:
-    reg rotation: (左(37,+angleInc), 右(39,-angleInc), 上(40,-speed), 下(38,+speed))
-    */
-    char.posXY(); // updates 2d location  
-    char.posDeg(); // updates rotation factor
-    obstacle.update();
+    if (char.crashWith(obA)) {
+        area.clear(); // so no duplicate char
+        obA.posXY();
+        obA.posDeg();   
+        char.angleInc = 0;
+        char.speed = 3;
+        if (area.keys && area.keys[32]) {
+            angleSpeed = 3 + inc;
+            char.angleInc = angleSpeed;
+            inc += 0.1; // the longer area.keys[32], the faster char spins
+        } else if (angleSpeed > 0) {
+            inc = 0;
+            angleSpeed -= 1;
+            char.angleInc = angleSpeed;
+        } 
+        char.posDeg(); // updates rotation factor
+        char.altPos(); // updates rotation orientation
+    } else { 
+        area.clear(); // so no duplicate char
+        obA.posXY();
+        obA.posDeg();   
+        char.angleInc = 0;
+        char.speed = 3;
+        if (area.keys && area.keys[32]) {
+            angleSpeed = 3 + inc;
+            char.angleInc = angleSpeed;
+            inc += 0.1; // the longer area.keys[32], the faster char spins
+        } else if (angleSpeed > 0) {
+            inc = 0;
+            angleSpeed -= 1;
+            char.angleInc = angleSpeed;
+        } 
+        char.posXY(); // updates 2d location  
+        char.posDeg(); // updates rotation factor
+    }
 }
 
 window.onkeydown = function(e) {
-    if(e.keyCode == 32 && e.target == document.body) {
+    if (e.keyCode == 32 && e.target == document.body) {
         e.preventDefault();
         return false;
     }
